da0ffeebe313ca8b7afad5562d96d8bbecfb6a0d
Dhanesh Saini <sainidhanesh58@gmail.com>
2024-06-27 07:54:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import MyContext from './Context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MyContext>
         <App />
       </MyContext>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
